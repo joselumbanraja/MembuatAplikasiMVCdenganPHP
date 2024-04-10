e3f0363e089bf4176cd993a020e2d0c4d42371e8
joselumbanraja <lumbanrajajose12@gmail.com>
2024-04-10 10:55:48 +0700

    My first file

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://127.0.0.1:4308/phpmvc/public');
+
+// DB
+define('DB_HOST', '127.0.0.1:4308');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
---
+++ app/models/User_model.php
@@ -0,0 +1,11 @@
+<?php
+
+class User_model
+{
+    private $nama = 'Doddy Ferdiansyah';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
